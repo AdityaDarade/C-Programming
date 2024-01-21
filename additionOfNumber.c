f3cf33a8bd5732ba7bed8a3df84c9877be571181
AdityaDarade <150779640+AdityaDarade@users.noreply.github.com>
2024-01-21 14:21:18 +0530

    additionOfNumber

---
+++ additionOfNumber.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int x = 5;
+    int output;
+    
+    printf("%d\n",x);
+    printf("%d\n",output);
+    
+    output= x+10;
+    printf("%d\n",x);
+    printf("%d\n",output);
+    
+}
