b57c95f1796fd24304dfef8cf6a19130f31e5baf
Aditya Darade <150779640+AdityaDarade@users.noreply.github.com>
2024-01-29 14:31:26 +0530

    If sterment
    In this code If is use for first time.
    #c programing

---
+++ If statement.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+    printf("start main\n");
+
+    char ch1 = 'A';
+    char ch2 = '48';
+    
+    if(ch1)
+    {
+        printf("In first If block\n");
+         }
+         printf("ch2 = %c \n",ch2);
+    if(ch2)
+    {
+      printf("In second If block\n");
+    }
+    
+    printf("End main\n");
+    
+}
