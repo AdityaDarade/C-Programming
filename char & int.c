9aa43878aa3d8d93b375ec15cec9f30e0b746cf2
Aditya Darade <150779640+AdityaDarade@users.noreply.github.com>
2024-01-30 15:41:29 +0530

    Add files via upload

---
+++ char & int.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int x = 10;
+    char ch1 = 'A';
+    char ch2 = 97;
+    
+    printf("%d \n",x);
+    printf("%c \n",ch1);
+    printf("%c \n",ch2);
+    
+}
