b0484439aed77bdfef831148af37354a37c891ef
Aditya Darade <150779640+AdityaDarade@users.noreply.github.com>
2024-01-30 15:34:19 +0530

    If Statement  2

---
+++ If Statement2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main(){
+     float income;
+     printf("Enter your Salary\n");
+     scanf("%f",&income);
+     
+     if(income >=0 && income<=500000);{
+     printf("Tax slab is 0%\n");
+     }
+      if(income >=500000 && income<=1000000);{
+     printf("Tax slab is 0%\n");
+     }
+      if(income >=1000000 && income<=1500000);{
+     printf("Tax slab is 0%\n");
+     }
+}
