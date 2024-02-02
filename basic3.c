3be69fd553ce8e1bdaf42ba1031565f0a79507ba
Aditya Darade <150779640+AdityaDarade@users.noreply.github.com>
2024-02-02 12:19:56 +0530

    Basic code3.c
    In This Code, scanf function is used

---
+++ basic3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main(){
+   int x,y;
+     printf("Enter the value\n");
+     scanf("%d",&x);
+     printf("is divisible by 5 & 11\n") ;
+              printf("Enter the value\n");
+              scanf("%d",&y);
+       printf("is divisible by 5 & 11\n");
+}
+   
+   
