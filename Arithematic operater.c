809145c94e8b85d8155e2de325b66a629c2400d7
Aditya Darade <150779640+AdityaDarade@users.noreply.github.com>
2024-02-13 17:59:01 +0530

    Arithemetic operater.c

---
+++ Arithematic operater.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main(){
+      int A,B;
+      printf("Enter the integar:\n");
+      scanf("%d%d",&A,&B);
+      printf("%d\n",A+B );
+      printf("%d\n",A-B );
+      printf("%d\n",A/B );
+      printf("%d\n",A*B );
+       printf("%d\n",A%B );
+}
