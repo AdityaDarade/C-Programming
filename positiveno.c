fe34f71e5f7c44a62ab4790e0cfeee68923877d7
Aditya Darade <150779640+AdityaDarade@users.noreply.github.com>
2024-02-16 22:25:49 +0530

    Positive & Negative Number

---
+++ positiveno.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main(){
+    int A;
+    printf("Enter the Number:\n");
+    scanf("%d",&A);
+    
+    if(A > 0)
+    {
+        printf("This is positive\n");
+        
+    }
+    else if(A==0){
+    printf("this is zero");
+    }
+    else{
+        printf("this is negative");
+    }
+
+    
+    
+    
+    
+}
