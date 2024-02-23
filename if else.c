4182492755c273938a5a5a163b9595eba8c8f1f3
Aditya Darade <150779640+AdityaDarade@users.noreply.github.com>
2024-02-23 13:59:09 +0530

    if else.c

---
+++ if else.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main(){
+  int x;
+  printf("Enter the value:\n");
+  scanf("%d",&x);
+
+  if(10 < x){
+      printf("True");
+  }
+  else if(10==10){
+      printf("false");
+
+  }else{
+      printf("false");
+  }
+
+}
