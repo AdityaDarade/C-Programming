424c269afee511fa861746a415c82746e272e6d6
Aditya Darade <150779640+AdityaDarade@users.noreply.github.com>
2024-02-24 22:11:41 +0530

    Swaping.c

---
+++ swaping.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main(){
+       int x=10;
+       int y=20;
+       printf("%d\n",x);
+       printf("%d\n",y);
+     int temp;
+   temp=x;
+   x=y;
+   y=temp;
+    printf("%d\n",x);
+    printf("%d\n",y);
+    
+}
