185df43f250142f32280389b8ffcedb413529c2f
Aditya Darade <150779640+AdityaDarade@users.noreply.github.com>
2024-02-29 19:56:14 +0530

    2D array2.c

---
+++ 2D-array2.c
@@ -0,0 +1,13 @@
+//This code is of 2D array(Two dimentional array)
+#include<stdio.h>
+void main(){
+    int arr[2][3]={1,2,3,4};
+   
+   int arr2[3][3]={{1,2,3},{4,5},{6}};
+   
+   printf("%d\n",arr[1][3]);
+   printf("%d\n",arr[2][3]);
+  printf("%d\n",arr[0][0]);
+  printf("%d\n",arr[1][3]);
+  printf("%d\n",arr2[3][3]);
+}
